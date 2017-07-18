b144bd4b9856d1c65c63b4caf65bebb61c183bc1
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-07-18 22:08:57 +0000

    refactored to separate output into 2 other functions from client.query (input) function

--- lookup_people.js
+++ lookup_people.js
@@ -10,19 +10,28 @@ const client = new pg.Client({
   ssl      : settings.ssl
 });
 
+const printPerson = (result) => {
+  console.log(`Found ${result.rows.length} person(s) by the name ${process.argv.slice(2)[0]}:`);
+};
+
+const printPeople = (result) => {
+  result.rows.forEach((result) => {
+      console.log(`- ${result.id}: ${result.first_name} ${result.last_name}, born '${result.birthdate.toString().substring(4, 15)}'`);
+    });
+};
+
+
 client.connect((error) => {
-  const input = process.argv.slice(2)[0];
   if (error) {
     return console.error("Connection Error", error);
   }
   console.log("Searching...");
-  client.query("SELECT * FROM famous_people WHERE (first_name = $1::text OR last_name = $1::text)", [input], (error, result) => {
+  client.query("SELECT * FROM famous_people WHERE (first_name = $1::text OR last_name = $1::text)", [process.argv.slice(2)[0]], (error, result) => {
     if (error) {
       return console.error("error running query", error);
     };
-    console.log(`Found ${result.rows.length} person(s) by the name ${input}:`);
-    console.log(`- ${result.rows[0].id}: ${result.rows[0].first_name} ${result.rows[0].last_name}, born '${result.rows[0].birthdate.toString().substring(4, 15)}'`);
-
+    printPerson(result);
+    printPeople(result);
     client.end();
   });
 });
